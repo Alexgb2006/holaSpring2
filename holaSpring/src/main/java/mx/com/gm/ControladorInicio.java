1735f62b5c0ca66bb646a1e3da8117ea41a208c4
alexg <alexgb2006@msn.com>
2022-06-18 02:58:22 -0500

    Funcionando el autoreload

--- holaSpring/src/main/java/mx/com/gm/ControladorInicio.java
+++ holaSpring/src/main/java/mx/com/gm/ControladorInicio.java
@@ -1,18 +1,22 @@
 package mx.com.gm;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
+@Controller
 @Slf4j
 public class ControladorInicio {
 
     @GetMapping("/")
-    public String inicio(){
-        log.info("Ejecutando el controlador rest");
-        log.debug("Mas detalle");
-        return "Hola Mundo 2";
+    public String inicio(Model model){
+        var saludar = "Hola Mundo con Thymeleaf";
+        log.info("Ejecutando el controlador Spring MVC");
+        model.addAttribute("mensaje",log);
+
+        return "index";
 
     }
 
