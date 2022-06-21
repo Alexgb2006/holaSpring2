c2a69ed09e3601f85bf8ed032f695b5859027b50
alexg <alexgb2006@msn.com>
2022-06-20 19:19:52 -0500

    Funcionando el autoreload2

--- holaSpring/src/main/java/mx/com/gm/ControladorInicio.java
+++ holaSpring/src/main/java/mx/com/gm/ControladorInicio.java
@@ -1,6 +1,7 @@
 package mx.com.gm;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,11 +11,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Slf4j
 public class ControladorInicio {
 
+    @Value("${index.saludo}")
+    private String saludo;
+
     @GetMapping("/")
     public String inicio(Model model){
-        var saludar = "Hola Mundo con Thymeleaf";
+        var mensaje = "Mensajes con Thymeleaf";
         log.info("Ejecutando el controlador Spring MVC");
-        model.addAttribute("mensaje",log);
+        model.addAttribute("mensaje",mensaje);
+        model.addAttribute("saludo",saludo);
 
         return "index";
 
